d0380eb0339594c21af5d49ff44c18b2de1097fd
Kumar <kumarpndt12@gmail.com>
2021-08-30 13:12:37 +1000

    final solution

--- quizAppLwc.js
+++ quizAppLwc.js
@@ -1,7 +1,9 @@
 import { LightningElement } from 'lwc';
 
 export default class QuizAppLwc extends LightningElement {
-
+    selected={}
+    correctAnswers=0;
+    isSubmitted = false;
     myQuestions=[
         {
             id:"Question1",
@@ -34,9 +36,34 @@ export default class QuizAppLwc extends LightningElement {
             correctAnswer:"c"
         }
     ]
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length ===this.myQuestions.length);
+    }
+
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers?
+            'slds-text-color_success':'slds-text-color_error'}`
+    }
 
     changeHandler(event){
         console.log("name", event.target.name);
         console.log("value", event.target.value);
+        //equivalent to const name = evenet.target.name
+        const {name,value} = event.target;
+        this.selected={...this.selected, [name]:value}
+        
     }
+    submitHandler(event){
+        event.preventDefault()
+        let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+        this.correctAnswers = correct.length;
+        this.isSubmitted=true;
+        console.log("this.correctAnswers ", this.correctAnswers);
+    }
+    resetHandler(){
+        this.selected={}
+        this.correctAnswers=0;
+        this.isSubmitted=false;
+    }
+
 }
